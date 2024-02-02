34ffd6bb66dd2e0f52f2718612b1af572e37e0f8
muhannadalbakkar <115506911+muhannadalbakkar@users.noreply.github.com>
2024-02-02 01:24:35 +0100

    Source of the dataset has been added

--- Covid Portfolio Project.sql
+++ Covid Portfolio Project.sql
@@ -1,5 +1,5 @@
-
---select Data that we ar going to be using
+-- Source of the dataset is Ourworldindata.org/covid-deaths
+-- select Data that we ar going to be using
 
 Select location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
